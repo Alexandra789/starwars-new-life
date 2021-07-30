2210a4e091aa439fb61161c6e7357a8373a35cf1
Alexandra789 <zakovinko99@gmail.com>
2021-07-30 12:49:23 +0300

    add header and categories components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Header from '../Header/Header';
+import Categories from "../Categories/Categories";
+import AllObjectsContainer from "../AllObjectsContainer/AllObjectsContainer";
+import React, { useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    const [category, setCategory] = useState('people');
+    return (
+        <div className="App">
+            <Header/>
+            {/*<Categories setCategory={setCategory}/>*/}
+            <AllObjectsContainer category={category}/>
+        </div>
+    );
 }
 
 export default App;
