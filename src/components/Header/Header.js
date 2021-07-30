eee86349d26d23f36f854c20d38e809574e707af
Alexandra789 <zakovinko99@gmail.com>
2021-07-30 12:47:53 +0300

    add logo

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+export default function Header() {
+    return (
+        <header className = "header">
+        <div className = "container">
+            <div className = "logo">
+                <div className = "title logo__title">star</div>
+                <div className="subtitle logo__subtitle">A VISUAL GUIDE</div>
+                <div className = "title logo__title">wars</div>
+            </div>
+        </div>
+        </header>
+    )
+}
